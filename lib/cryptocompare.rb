3b21124a9e1836c0020542ff34837c89cca8a040
Rishat <81845@mail.ru>
2023-03-31 18:30:55 +0300

    add configuration module

--- lib/cryptocompare.rb
+++ lib/cryptocompare.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "cryptocompare/version"
+require_relative "cryptocompare/configuration"
 
+# Main module
 module Cryptocompare
-  class Error < StandardError; end
-  # Your code goes here...
+  extend Configuration
 end
---
+++ spec/cryptocompare/configuration_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Cryptocompare
+  RSpec.describe Configuration do
+    let(:dummy_class) { Class.new { extend Configuration } }
+
+    describe "#configuration" do
+      before do
+        dummy_class.configuration do |config|
+          config.logger = "logger"
+          config.api_key = "api_key"
+          config.try_conversion = "try_conversion"
+          config.relaxed_validation = "relaxed_validation"
+          config.e = "e"
+          config.extra_params = "extra_params"
+          config.sign = "sign"
+          config.pure_hash = "pure_hash"
+        end
+      end
+
+      it { expect(dummy_class.logger).to eq("logger") }
+      it { expect(dummy_class.api_key).to eq("api_key") }
+      it { expect(dummy_class.try_conversion).to eq("try_conversion") }
+      it { expect(dummy_class.relaxed_validation).to eq("relaxed_validation") }
+      it { expect(dummy_class.e).to eq("e") }
+      it { expect(dummy_class.extra_params).to eq("extra_params") }
+      it { expect(dummy_class.sign).to eq("sign") }
+      it { expect(dummy_class.pure_hash).to eq("pure_hash") }
+    end
+  end
+end
