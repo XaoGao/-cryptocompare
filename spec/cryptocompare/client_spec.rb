69fc1adf69b61b0b3f18bfdcd9d267ac3a2035fe
Rishat <81845@mail.ru>
2023-07-07 20:30:40 +0300

    add spec for client.get, fix readme

--- spec/cryptocompare/client_spec.rb
+++ spec/cryptocompare/client_spec.rb
@@ -5,6 +5,31 @@ module Cryptocompare
     let(:options) { { api_key: "123" } }
     let(:client) { described_class.new(options) }
 
+    describe "#get" do
+      before do
+        described_class.configuration do |config|
+          config.api_key = "api_key"
+          config.try_conversion = "try_conversion"
+          config.relaxed_validation = "relaxed_validation"
+          config.e = "e"
+          config.extra_params = "extra_params"
+        end
+      end
+
+      it "create instance" do
+        cleint = described_class.get(api_key: "123")
+
+        expect(cleint.send(:options)[:api_key]).to eq("123")
+      end
+
+      it "one example" do
+        cleint1 = described_class.get(api_key: "123")
+        client2 = described_class.get(api_key: "456")
+
+        expect(cleint1.object_id).to eq(client2.object_id)
+      end
+    end
+
     describe ".convert" do
       context "when success response" do
         let(:body) { JSON.dump("ETH" => 15.23, "USD" => 29_843.3) }
