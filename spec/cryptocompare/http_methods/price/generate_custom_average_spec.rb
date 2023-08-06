c09c9fa1d9fde99e691a942e0b2a6ee6a10a4348
Rishat <81845@mail.ru>
2023-08-06 12:58:30 +0300

    fix: rename tsyms -> tsym and change type array-> string

--- spec/cryptocompare/http_methods/price/generate_custom_average_spec.rb
+++ spec/cryptocompare/http_methods/price/generate_custom_average_spec.rb
@@ -16,42 +16,42 @@ module Cryptocompare
         describe ".perform" do
           context "when options is invalid" do
             it "fsym is not set" do
-              options_without_fsym = options.merge(tsyms: %w[ETH USD], e: "Coinbase,Bitfinex")
+              options_without_fsym = options.merge(tsym: "USD", e: "Coinbase,Bitfinex")
               expect { generate_custom_average.perform(options_without_fsym) }.to raise_error(ArgumentError, "fsym can not be nil")
             end
 
-            it "tsyms is not set" do
-              options_without_tsyms = options.merge(fsym: "ETH", e: "Coinbase,Bitfinex")
-              expect { generate_custom_average.perform(options_without_tsyms) }.to raise_error(ArgumentError, "tsyms can not be nil")
+            it "tsym is not set" do
+              options_without_tsym = options.merge(fsym: "ETH", e: "Coinbase,Bitfinex")
+              expect { generate_custom_average.perform(options_without_tsym) }.to raise_error(ArgumentError, "tsym can not be nil")
             end
 
             it "fsym is not a string" do
-              options_fsym_is_not_string = options.merge(fsym: 10, tsyms: %w[ETH USD], e: "Coinbase,Bitfinex")
+              options_fsym_is_not_string = options.merge(fsym: 10, tsym: "USD", e: "Coinbase,Bitfinex")
               expect { generate_custom_average.perform(options_fsym_is_not_string) }.to raise_error(ArgumentError, "fsym must be a string")
             end
 
-            it "tsyms is not an array" do
-              options_fsym_is_not_array = options.merge(fsym: "ETH", tsyms: "USD", e: "Coinbase,Bitfinex")
-              expect { generate_custom_average.perform(options_fsym_is_not_array) }.to raise_error(ArgumentError, "tsyms must be an array")
+            it "tsym is not an array" do
+              options_tsym_is_not_string = options.merge(fsym: "ETH", tsym: 10, e: "Coinbase,Bitfinex")
+              expect { generate_custom_average.perform(options_tsym_is_not_string) }.to raise_error(ArgumentError, "tsym must be a string")
             end
 
             it "e is a null" do
-              options_e_is_null = options.merge(fsym: 10, tsyms: %w[ETH USD])
+              options_e_is_null = options.merge(fsym: 10, tsym: "USD")
               expect { generate_custom_average.perform(options_e_is_null) }.to raise_error(ArgumentError, "e can not be nil")
             end
 
             it "e is not a string" do
-              options_e_is_not_string = options.merge(fsym: "ETH", tsyms: %w[ETH USD], e: 1)
+              options_e_is_not_string = options.merge(fsym: "ETH", tsym: "USD", e: 1)
               expect { generate_custom_average.perform(options_e_is_not_string) }.to raise_error(ArgumentError, "e must be a string")
             end
           end
 
           context "when success response" do
             let(:body) { JSON.dump("ETH" => 15.23, "USD" => 29_843.3) }
-            let(:response) { generate_custom_average.perform(fsym: "BTC", tsyms: %w[ETH USD], e: "Coinbase,Bitfinex") }
+            let(:response) { generate_custom_average.perform(fsym: "BTC", tsym: "USD", e: "Coinbase,Bitfinex") }
 
             before do
-              stub_request(:get, "https://min-api.cryptocompare.com/data/generateAvg?apiKey=123&fsym=BTC&tsyms=ETH,USD&e=Coinbase,Bitfinex")
+              stub_request(:get, "https://min-api.cryptocompare.com/data/generateAvg?apiKey=123&fsym=BTC&tsym=USD&e=Coinbase,Bitfinex")
                 .with(headers: { "Accept" => "*/*", "Content-Type" => "application/json" })
                 .to_return(status: 200, body:, headers: {})
             end
@@ -60,6 +60,40 @@ module Cryptocompare
             it { expect(response.status).to be true }
             it { expect(response.error).to be_nil }
             it { expect(response.value.env.response_body).to eq(body) }
+          end
+        end
+
+        describe ".perform!" do
+          context "when options is invalid" do
+            it "fsym is not set" do
+              options_without_fsym = options.merge(tsym: "USD", e: "Coinbase,Bitfinex")
+              expect { generate_custom_average.perform(options_without_fsym) }.to raise_error(ArgumentError, "fsym can not be nil")
+            end
+
+            it "tsym is not set" do
+              options_without_tsym = options.merge(fsym: "ETH", e: "Coinbase,Bitfinex")
+              expect { generate_custom_average.perform(options_without_tsym) }.to raise_error(ArgumentError, "tsym can not be nil")
+            end
+
+            it "fsym is not a string" do
+              options_fsym_is_not_string = options.merge(fsym: 10, tsym: "USD", e: "Coinbase,Bitfinex")
+              expect { generate_custom_average.perform(options_fsym_is_not_string) }.to raise_error(ArgumentError, "fsym must be a string")
+            end
+
+            it "tsym is not an array" do
+              options_tsym_is_not_string = options.merge(fsym: "ETH", tsym: 10, e: "Coinbase,Bitfinex")
+              expect { generate_custom_average.perform(options_tsym_is_not_string) }.to raise_error(ArgumentError, "tsym must be a string")
+            end
+
+            it "e is a null" do
+              options_e_is_null = options.merge(fsym: 10, tsym: "USD")
+              expect { generate_custom_average.perform(options_e_is_null) }.to raise_error(ArgumentError, "e can not be nil")
+            end
+
+            it "e is not a string" do
+              options_e_is_not_string = options.merge(fsym: "ETH", tsym: "USD", e: 1)
+              expect { generate_custom_average.perform(options_e_is_not_string) }.to raise_error(ArgumentError, "e must be a string")
+            end
           end
         end
       end
